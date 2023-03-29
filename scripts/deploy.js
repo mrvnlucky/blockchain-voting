c81f5bdf02aa8bbc706cb0846c3c84073405e15d
Marvin <marvnluck@gmail.com>
2023-03-29 08:16:48 +0700

    Added smart contract for voting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Contract = await hre.ethers.getContractFactory("MyContract");
+  const Contract = await hre.ethers.getContractFactory("Voting");
   const contract = await Contract.deploy();
 
   await contract.deployed();
