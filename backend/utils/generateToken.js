c0e2cbcb265deb4031e33f369257e82a89d2c9fa
Marvin Luckianto <marvnluck@gmail.com>
2023-09-26 21:57:20 +0700

    split frontend and backend

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-
-exports.generateToken = (data) => {
-  const payload = { data };
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-};
